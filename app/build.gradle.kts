aff62021525b896eaa547a007baca5e9a0c3ce42
amethyst <amethyst@example.com>
2026-07-01 08:26:41 +0500

    full fix project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,18 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
